0b1a47f1f424f03b6f4e382ad6c1b25748b5028c
Alex Vranas <avranas42@gmail.com>
2024-12-01 09:20:26 -0800

    day 1 complete

---
+++ day1/day1-1.py
@@ -0,0 +1,17 @@
+import heapq
+
+with open('day1input.txt', 'r') as file:
+    nums = [s for s in file.read().split() if s]
+
+left, right = [], []
+for i, num in enumerate(nums):
+  heapq.heappush(left if i % 2 == 0 else right, int(num))
+  
+total = 0
+
+while left or right:
+  lNum = heapq.heappop(left)
+  rNum = heapq.heappop(right)
+  total += abs(lNum - rNum)
+
+print(total)
---
+++ day1/day1-2.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+with open('day1input.txt', 'r') as file:
+    nums = [s for s in file.read().split() if s]
+
+left = [int(num) for i, num in enumerate(nums) if i % 2 == 0]
+right = [int(num) for i, num in enumerate(nums) if i % 2 == 1]
+rightCounter = Counter(right)
+total = 0
+
+for num in left:
+  total += rightCounter[num] * num
+  
+print(total)
